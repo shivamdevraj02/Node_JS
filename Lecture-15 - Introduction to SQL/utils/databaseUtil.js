52db9617235b7f0057e4b31ef078868323ca2b08
Devraj <devrajshivam02@gmail.com>
2026-06-05 18:24:28 +0530

    new password set

--- Lecture-15 - Introduction to SQL/utils/databaseUtil.js
+++ Lecture-15 - Introduction to SQL/utils/databaseUtil.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Completecoding@01',
+  password: 'Devraj@1372',
   database: 'airbnb'
 });
 
